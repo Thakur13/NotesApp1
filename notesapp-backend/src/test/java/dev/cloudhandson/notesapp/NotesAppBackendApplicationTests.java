cfd6e9997de737db986e21ca5793530df25d4987
Thakur13 <asthakur185@gmail.com>
2023-08-18 00:27:05 +0530

    Added spring-boot setup

---
+++ notesapp-backend/src/test/java/dev/cloudhandson/notesapp/NotesAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cloudhandson.notesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesiAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
